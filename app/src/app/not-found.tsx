4d7154d3220b96090ec87e590dcc1b7849aac09d
Steffin Rayen <157079042+steffinhustles@users.noreply.github.com>
2024-02-09 02:47:36 +0000

    🎨 404 linking

--- app/src/app/not-found.tsx
+++ app/src/app/not-found.tsx
@@ -12,14 +12,14 @@ export default function NotFound() {
             <div className="grid place-content-center">
                 <div className="text-center">
 
-                    <h1 className="text-9xl font-black text-gray-200 dark:text-gray-700">404</h1>
+                    <h1 className="text-9xl font-black text-gray-200 dark:text-lemon">404</h1>
                     <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-white">
                         Uh-oh!
                     </p>
                     <p className="mt-4 text-gray-500 dark:text-gray-400">We can't find that page.</p>
                     <a
                         href="/"
-                        className="mt-6 inline-block rounded bg-lemon px-5 py-3 text-sm font-bold text-brown hover:bg-yellow focus:outline-none focus:ring"
+                        className="mt-6 inline-block rounded bg-brown px-5 py-3 text-sm font-bold text-lemon hover:bg-yellow hover:text-brown focus:outline-none focus:ring"
                     >
                         Go Back Home
                     </a>
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,27 +3,27 @@ import Image from "next/image";
 export default function Home() {
   const listOfCategories = [
     {
-      "link": "#",
+      "link": "/1",
       "title": "Category 1",
       "caption": "Category 1 line description.",
     }, {
-      "link": "#",
+      "link": "/2",
       "title": "Category 2",
       "caption": "Category 2 line description.",
     }, {
-      "link": "#",
+      "link": "/3",
       "title": "Category 3",
       "caption": "Category 3 line description.",
     }, {
-      "link": "#",
+      "link": "/4",
       "title": "Category 4",
       "caption": "Category 4 line description.",
     }, {
-      "link": "#",
+      "link": "/5",
       "title": "Category 5",
       "caption": "Category 5 line description.",
     }, {
-      "link": "#",
+      "link": "/6",
       "title": "Category 6",
       "caption": "Category 6 line description.",
     }
@@ -32,14 +32,17 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24">
       {/* header */}
       <div className="z-10 mb-16">
-        <Image src="/logo.png" alt="Thread Therapy Logo" width={200} height={200} priority />
+        <a href="/about">
+          <Image src="/logo.png" alt="Thread Therapy Logo" width={200} height={200} priority />
+        </a>
       </div>
       {/* list of link cards */}
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-12 lg:grid-cols-4 lg:text-left">
         {listOfCategories.map((category, index) => (
           <a href={category.link} key={index}
             className="group rounded-lg border border-transparent px-5 py-4 mb-8 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank" rel="noopener noreferrer">
+            // target="_blank" rel="noopener noreferrer"
+            >
             <h2 className={`mb-3 text-2xl font-semibold`}>
               {category.title}{" "}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">

6bca3a59c270ac411d3d6c646eb4bf69c86e07ab
Steffin Rayen <157079042+steffinhustles@users.noreply.github.com>
2024-02-09 02:24:26 +0000

    💄 Added Theme Colors

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -8,10 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'brown': '#744838',
+        'yellow': '#F4C524',
+        'lemon': '#FFFAC8',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        
       },
     },
   },

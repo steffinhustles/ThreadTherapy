bcf31a5fa101b5e3b5666aa66ff4ad33941f6367
Steffin Rayen <157079042+steffinhustles@users.noreply.github.com>
2024-02-09 02:39:28 +0000

    🥅 404 page :)

---
+++ app/src/app/not-found.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+export default function NotFound() {
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24">
+            {/* header */}
+            <div className="z-10 mb-16">
+                <a href="/about">
+                    <Image src="/logo.png" alt="Thread Therapy Logo" width={200} height={200} priority />
+                </a>
+            </div>
+            {/* body */}
+            <div className="grid place-content-center">
+                <div className="text-center">
+
+                    <h1 className="text-9xl font-black text-gray-200 dark:text-gray-700">404</h1>
+                    <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-white">
+                        Uh-oh!
+                    </p>
+                    <p className="mt-4 text-gray-500 dark:text-gray-400">We can't find that page.</p>
+                    <a
+                        href="/"
+                        className="mt-6 inline-block rounded bg-lemon px-5 py-3 text-sm font-bold text-brown hover:bg-yellow focus:outline-none focus:ring"
+                    >
+                        Go Back Home
+                    </a>
+                </div>
+
+            </div>
+        </main>
+    )
+}
